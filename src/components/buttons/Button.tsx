9f73f17dd3fe940c274fda88f4014d45ed64d948
Maria Kazakova <mariakazakova92@gmail.com>
2024-06-04 16:46:42 +0300

    refactored Butten component

--- src/components/buttons/Button.tsx
+++ src/components/buttons/Button.tsx
@@ -1,66 +1,44 @@
+import {ButtonHTMLAttributes, ReactNode} from "react";
 import styled from "styled-components";
-import {theme} from "../../styles/Theme";
+
+
+type ButtonProps = {
+    children?: ReactNode,
+    backgroundColor?: string,
+    border?: string
+    color: string
+} & ButtonHTMLAttributes<HTMLButtonElement>
+
+export const Button = ({
+                           title,
+                           onClick,
+                           children,
+                           color,
+                           backgroundColor,
+                           className,
+                           border,
+                           ...restProps
+                       }: ButtonProps) => {
+    return <StyledButton title={title}
+                         onClick={onClick}
+                         color={color}
+                         backgroundColor={backgroundColor}
+                         border={border}
+    >{children}</StyledButton>
+};
 
 type ButtonPropsStyle = {
     backgroundColor?: string,
     color: string,
     border?: string
-
 }
 
-export const Button = styled.button<ButtonPropsStyle>`
-    min-width: 122px;
-    min-height: 63px;
+const StyledButton = styled.button<ButtonPropsStyle>`
     color: ${props => props.color};
     background-color: ${props => props.backgroundColor || 'transparent'};
-    border: ${props => props.border || `solid 1.5px ${theme.colors.accentColor}`};
-    padding: 12px 21px;
+    border: ${props => props.border || 'none'};
+    padding: 20px 26px;
     border-radius: 18px;
     font-weight: 700;
     font-size: 18px;
-
-    // :hover {
-    //     background-color:${theme.colors.accentColor};
-    //     color: ${theme.colors.secondaryColor};
-    // }
 `
-
-// import {ButtonHTMLAttributes, ReactNode} from "react";
-// import styled from "styled-components";
-// import {theme} from "../../styles/Theme";
-//
-// type ButtonProps = {
-//     children?: ReactNode,
-//     backgroundColor?: string,
-//     border?: string
-// } & ButtonHTMLAttributes<HTMLButtonElement>
-//
-// export const Button = ({
-//                            title,
-//                            onClick,
-//                            children,
-//                            color,
-//                            backgroundColor,
-//                            className,
-//                            border,
-//                            ...restProps
-//                        }: ButtonProps) => {
-//     return <StyledButton title={title}
-//                          onClick={onClick}
-//                          color={color}
-//                          backgroundColor={backgroundColor}
-//                          border={border}
-//     >{children}</StyledButton>
-// };
-//
-// const StyledButton = styled.button`
-//     //min-width: 122px;
-//     //min-height: 63px;
-//     color: ${props => props.color};
-//     background-color: ${props => props.backgroundColor || 'transparent'};
-//     border: ${props => props.border || `solid 1.5px ${theme.colors.accentColor}`};
-//     padding: 12px 21px;
-//     border-radius: 18px;
-//     font-weight: 700;
-//     font-size: 18px;
-// `
